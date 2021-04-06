c720d06b2a72a7c79643827d66c0481464e2fc53
sengdu <sengdu005416@gmail.com>
2021-04-06 16:02:53 +0630

    translate-button

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class App extends React.Component{
-	state = {language: 'english'};
-	onLanguageChange = language =>{
+	state = { language: 'english' };
+
+	onLanguageChange = language => {
 		this.setState({ language });
-	}
+	};
 
 	render(){
 		return (
 			<div className="ui container">
 				<div>
-					<h2>Select a language:</h2>
-					<i className="flag us" onClick={() => this.onLanguageChange('english')}></i>
-					<i className="flag myanmar" onClick={() =>this.onLanguageChange('myanmar')}></i>
+					Select a language:
+					<i className="flag us" onClick={() => this.onLanguageChange('english')} />
+					<i className="flag nl" onClick={() =>this.onLanguageChange('dutch')} />
 				</div>
-				{this.state.language}
+				<ColorContext.Provider value="red">
+                   <LanguageContext.Provider value= {this.state.language}>
+                       < UserCreate />
+                    </LanguageContext.Provider>
+                </ColorContext.Provider>
 			</div>
 		)
 	}
