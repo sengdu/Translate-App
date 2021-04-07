36b3c122252f04183efe8b0affbe1a7e61f98487
sengdu <sengdu005416@gmail.com>
2021-04-07 22:21:22 +0630

    redux-vs-context

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,28 @@
 import React from 'react';
 import UserCreate from './UserCreate';
-import LanguageContext from '../contexts/LanguageContext';
+import {LanguageStore} from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from './LanguageSelector';
 
-class App extends React.Component{
-	state = { language: 'english' };
+class App extends React.Component {
+  state = { language: 'english' };
 
-	onLanguageChange = language => {
-		this.setState({ language });
-	};
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
 
-	render(){
-		return (
-			<div className="ui container">
-				<div>
-					Select a language:
-					<i className="flag us" onClick={() => this.onLanguageChange('english')} />
-					<i className="flag nl" onClick={() =>this.onLanguageChange('dutch')} />
-				</div>
-				<ColorContext.Provider value="red">
-                   <LanguageContext.Provider value= {this.state.language}>
-                       < UserCreate />
-                    </LanguageContext.Provider>
-                </ColorContext.Provider>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className="ui container">
+       <LanguageStore>
+	   <LanguageSelector />
+			<ColorContext.Provider value="red">
+				<UserCreate />
+			</ColorContext.Provider>
+	   </LanguageStore>
+      </div>
+    );
+  }
 }
 
 export default App;
